c744f3a627639f3fcfdf8f242a7d5e61a554985c
Blair Myhre <120442257+cocosbeans@users.noreply.github.com>
2024-09-04 13:07:14 -0500

    Update draw.js

--- src/draw.js
+++ src/draw.js
@@ -9,6 +9,39 @@ function border(matrix, val) {
     }
 }
 
+function fill(matrix, val) {
+    if (matrix instanceof Matrix) {
+        for (let i = 0; i < matrix.height; i++) {
+            matrix.setRow(i, val)
+        }
+    }
+}
+
+function rect(matrix, x, y, height, width, val) {
+    //Zero clue why this works, good luck reverse engineers
+    if (matrix instanceof Matrix) {
+        for (let i = y; i <= y + height - 1; i++) {
+            for (let j = x; j <= x + width - 1; j++) {
+                if (i === y || i === y + height - 1) matrix.set(j, i, val)
+                if (j === x || j === x + width - 1) matrix.set(j, i, val)
+            }
+        }
+    }
+}
+
+function rectFill(matrix, x, y, width, height, val) {
+    if (matrix instanceof Matrix) {
+        for (let i = y; i < height + y; i++) {
+            for (let j = x; j < width + x; j++) {
+                matrix.set(j, i, val)
+            }
+        }
+    }
+}
+
 module.exports = {
-    border
+    border,
+    fill,
+    rect,
+    rectFill
 }
