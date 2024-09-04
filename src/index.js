3638ca26763eb340cb7c0606ab48a3da5f6ac2c5
Blair Myhre <120442257+cocosbeans@users.noreply.github.com>
2024-09-04 13:31:44 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,23 @@
 const { Matrix } = require('./matrix.js')
+const { 
+    border,
+    fill,
+    rect,
+    rectFill,
+    render
+} = require('./draw.js')
 
-const { border } = require('./draw.js')
+const charset = [
+    '.', '@',
+    '/', '#',
+    '|', '-',
+    '+', '='
+]
 
-let mx = new Matrix(5, 5)
+let map = new Matrix(40, 10)
 
-border(mx, 9)
+border(map, 3)
+rectFill(map, 4, 3, 20, 3, 2)
 
-console.log(mx.matrix)
+//console.log(map.matrix)
+console.log(render(map, charset))
